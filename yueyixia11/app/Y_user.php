c0eeb67122fbfd74347758d7015ef0e214125075
ygx <810784427@qq.com>
2016-03-23 15:25:30 +0800

    remote yueyixia11

--- yueyixia11/app/Y_user.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Y_user extends Model {
-
-	//
-
-}
--- yueyixia11/public/Connect2.1/API/comm/inc.php
+++
@@ -1,2 +0,0 @@
-<?php die('forbidden'); ?>
-{"appid":"101295224","appkey":"3dc957fedee746c6915837d369e238bd","callback":"http://www.yueyixia.com/Connect2.1/example/oauth/callback.php","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
